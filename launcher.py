71cf872767d97c816cbee1b5365dfd33f89cd2a6
ShadowDog <101285259+shadowdog9500@users.noreply.github.com>
2022-03-09 22:42:53 +0100

    Added support for splash screen

--- launcher.py
+++ launcher.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2022 github.com/shadowdog9500 .
 #  License: MIT
-
+import importlib
 from PyQt5.QtWidgets import QApplication
 from src.ui_controller import ui_controller
 from src.ui.utils import errorMsgBox
@@ -9,6 +9,14 @@ import sys
 import os
 
 if __name__ == '__main__':
+    # Close PyInstaller Splash Image
+    # https://stackoverflow.com/a/68666505
+    if '_PYIBoot_SPLASH' in os.environ and importlib.util.find_spec("pyi_splash"):
+        import pyi_splash
+
+        pyi_splash.update_text('UI Loaded ...')
+        pyi_splash.close()
+
     config = configparser.ConfigParser()
     # Check if config file exists and load it or create it.
     if not os.path.isfile("config.ini"):
@@ -22,8 +30,11 @@ if __name__ == '__main__':
     # Check if py-patcher exists
     if not os.path.isfile("{}/launcher.exe".format(config['py-patcher']['path'])):
         errorMsgBox("Couldn't find the py-patcher executable. Make sure you've downloaded and "
-                    "unzip it in the location specified on the config.ini file.\n "
-                    "Current path: {}/launcher.exe".format(config['py-patcher']['path']))
+                    "unzip it in the location specified on the config.ini file.\n"
+                    "Current path: {}/launcher.exe\n\n"
+                    "py-patch download: https://github.com/illusion0001/py-patcher/releases".format(
+            config['py-patcher']['path']))
+
 
     else:
         # Load the UI
